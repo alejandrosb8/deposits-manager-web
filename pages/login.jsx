4e22effd6f7f859c000c471cafa63e886775f58b
Alejandro Sanchez <alejandrosanchezb08@gmail.com>
2022-09-07 04:58:19 -0400

    fix: syntax import

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import styles from './../styles/Login.module.css';
 import Image from 'next/image';
 import { useState } from 'react';
